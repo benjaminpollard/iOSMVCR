52bf03fe318bdd29941a15beb10d1ca7cf67e338
Benjamin Pollard <Benpollard1990@gmail.com>
2019-08-05 11:39:50 +0100

    add templat mvcr

---
+++ MVCR.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ : UIViewController 
+{
+		override func viewDidLoad() {
+        		super.viewDidLoad()
+        		// Do any additional setup after loading the view, typically from a nib.
+    		}
+}
